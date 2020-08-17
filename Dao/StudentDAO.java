4dbbdf57bc056c32f64c7c6f2730ae476bf02c31
wandering <2249655703@qq.com>
2020-08-17 21:42:19 +0800

    成绩管理系统

---
+++ Dao/StudentDAO.java
@@ -0,0 +1,39 @@
+package lliuyi.Dao;
+
+import lliuyi.model.Classes;
+import lliuyi.model.Course;
+import lliuyi.model.DictionaryTag;
+import lliuyi.model.Exam;
+import lliuyi.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+public class StudentDAO {
+    public static List<DictionaryTag> queryAsDict(int id) {
+        List<DictionaryTag> tags = new ArrayList<>();
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c=DBUtil.getConnection();
+            String sql = "select id,student_name from student where classes_id=?";
+            ps=c.prepareStatement(sql);
+            ps.setInt(1,id);
+            rs=ps.executeQuery();
+            while(rs.next()){
+                DictionaryTag tag = new DictionaryTag();
+                tag.setDictionaryTagKey(String.valueOf(rs.getInt("id")));
+                tag.setDictionaryTagValue(rs.getString("student_name"));
+                tags.add(tag);
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("查询学生数据字典出错",e);
+        }
+        return tags;
+    }
+}
---
+++ Dao/UserDAO.java
@@ -0,0 +1,36 @@
+package lliuyi.Dao;
+
+import lliuyi.model.DictionaryTag;
+import lliuyi.model.User;
+import lliuyi.util.DBUtil;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+public class UserDAO {
+
+    public static User query(User user) {
+        User queryUser=null;
+        Connection c = null;
+        PreparedStatement ps = null;
+        ResultSet rs = null;
+        try {
+            c=DBUtil.getConnection();
+            String sql = "select id,nickname from user where username=? and password=?";
+            ps=c.prepareStatement(sql);
+            ps.setString(1,user.getUsername());
+            ps.setString(2,user.getPassword());
+            rs=ps.executeQuery();
+            while(rs.next()){
+                queryUser=user;
+
+                queryUser.setId(rs.getInt("id"));
+                queryUser.setNickname(rs.getString("nickname"));
+            }
+        } catch (Exception e) {
+            throw new RuntimeException("用户登录出错",e);
+        }
+        return queryUser;
+    }
+}
