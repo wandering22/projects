d46a5c883f4f889b8382822ebdcd4e17d0d7fad1
wandering <2249655703@qq.com>
2020-08-25 00:07:42 +0800

    提交

---
+++ model/Page.java
@@ -0,0 +1,27 @@
+package lliuyi.model;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Getter
+@Setter
+@ToString
+public class Page {
+    private String searchText;//表格搜索的内容
+    private String sortOrder;//排序的方式
+    private Integer pageSize;//每页的数量
+    private Integer pageNumber;//当前页码
+
+    private Page(){}
+    public static Page parse(HttpServletRequest req){
+        Page p = new Page();
+        p.searchText = req.getParameter("searchText");
+        p.sortOrder = req.getParameter("sortOrder");
+        p.pageSize = Integer.parseInt(req.getParameter("pageSize"));
+        p.pageNumber = Integer.parseInt(req.getParameter("pageNumber"));
+        return p;
+    }
+}
