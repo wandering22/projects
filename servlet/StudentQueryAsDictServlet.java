3f83660952574a1da3cf0d6b1e81c357469ddaf2
wandering <2249655703@qq.com>
2020-08-24 23:09:11 +0800

    提交

---
+++ servlet/StudentQueryAsDictServlet.java
@@ -0,0 +1,19 @@
+package lliuyi.servlet;
+
+import lliuyi.Dao.StudentDAO;
+import lliuyi.model.DictionaryTag;
+
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+@WebServlet("/student/queryAsDict")
+public class StudentQueryAsDictServlet extends AbstractBaseServlet {
+    @Override
+    public Object process(HttpServletRequest req, HttpServletResponse resp) {
+        String id = req.getParameter("dictionaryKey");
+        List<DictionaryTag> tags = StudentDAO.queryAsDict(Integer.parseInt(id));
+        return tags;
+    }
+}
